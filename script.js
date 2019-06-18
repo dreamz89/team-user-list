20e8f4ba6f9beb0828ffa4fdbb7f8908dbcafa65
joctanys <joctanys@sph.com.sg>
2019-06-18 15:20:52 +0800

    retrieve search value

--- script.js
+++ script.js
@@ -4,5 +4,21 @@ fetch('https://jsonplaceholder.typicode.com/users')
   .catch(err => console.error(err))
 
 function searchData (data) {
-  console.log(data)
+  return data
+}
+
+const searchInput = document.getElementById('search')
+const searchButton = document.getElementById('button')
+
+// Trigger button click when the user presses enter
+searchInput.addEventListener('keyup', function (e) {
+  if (e.keyCode === 13) {
+    e.preventDefault()
+    searchButton.click()
+  }
+})
+
+function doSearch () {
+  const value = searchInput.value
+  searchInput.value = '' // reset field
 }
